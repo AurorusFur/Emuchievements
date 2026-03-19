3bf68d4d2be75d334aebdcebe17c8e78031bd5e4
Michał Cynka <firelol2004@gmail.com>
2026-03-19 20:18:13 +0100

    Added achievements to your stuff

--- src/ts/RoutePatches.tsx
+++ src/ts/RoutePatches.tsx
@@ -1,7 +1,9 @@
 import { RoutePatch, routerHook } from "@decky/api";
 import { Mountable } from "./System";
-import { EmuchievementsState } from "./hooks/achievementsContext";
+import { EmuchievementsState, EmuchievementsStateContextProvider } from "./hooks/achievementsContext";
 import { ReactElement } from "react";
+import { AchievementPageComponent } from "./components/achievementPageComponent";
+import { GamePageAchievementsSection } from "./components/gamePageAchievementsSection";
 
 function routePatch(path: string, patch: RoutePatch): Mountable
 {
@@ -17,11 +19,51 @@ function routePatch(path: string, patch: RoutePatch): Mountable
 	};
 }
 
-export function patchAppPage(_state: EmuchievementsState): Mountable
+export function patchAppPage(state: EmuchievementsState): Mountable
 {
 	// @ts-ignore
 	return routePatch("/library/app/:appid", (props: { path: string, children: ReactElement; }) =>
 	{
-		return props;
+		const match = window.location.href.match(/\/library\/app\/(\d+)/);
+		if (!match) return props;
+		const appId = parseInt(match[1], 10);
+
+		const overview = appStore.GetAppOverviewByAppID(appId);
+		if (!overview || overview.app_type !== 1073741824) return props;
+
+		return {
+			...props,
+			children: (
+				<>
+					{props.children}
+					<EmuchievementsStateContextProvider emuchievementsState={state}>
+						<GamePageAchievementsSection appId={appId} />
+					</EmuchievementsStateContextProvider>
+				</>
+			),
+		};
+	});
+}
+
+export function patchAchievementsPage(state: EmuchievementsState): Mountable
+{
+	// @ts-ignore
+	return routePatch("/library/app/:appid/achievements/my/individual", (props: { path: string, children: ReactElement; }) =>
+	{
+		const match = window.location.href.match(/\/library\/app\/(\d+)/);
+		if (!match) return props;
+		const appId = parseInt(match[1], 10);
+
+		const overview = appStore.GetAppOverviewByAppID(appId);
+		if (!overview || overview.app_type !== 1073741824) return props;
+
+		return {
+			...props,
+			children: (
+				<EmuchievementsStateContextProvider emuchievementsState={state}>
+					<AchievementPageComponent appId={appId} />
+				</EmuchievementsStateContextProvider>
+			),
+		};
 	});
 }
